9c487aea710d625ba89d0dd4a2496a221e3664df
kiwikid1543-alt <kiwikid1543@gmail.com>
2026-02-02 15:51:01 +0900

    feat: 할 일 항목 탭 시 빠른 연속 탭 방지를 위해 `TapDebouncer`를 적용했습니다.

--- lib/presentation/home/widgets/todo_view.dart
+++ lib/presentation/home/widgets/todo_view.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import 'package:tasks/presentation/detail/detail_page.dart';
 import 'package:tasks/presentation/home/widgets/to_do_widgets.dart';
 import 'package:tasks/presentation/home/home_view_model.dart';
+import 'package:tap_debouncer/tap_debouncer.dart';
 
 class TodoView extends ConsumerWidget {
   const TodoView({super.key});
@@ -14,22 +14,20 @@ class TodoView extends ConsumerWidget {
     return Expanded(
       // 컬럼에서 남은영역 차지할수있게함
       child: ListView.builder(
-        //
         itemCount: todos.length,
         itemBuilder: (context, index) {
-          return GestureDetector(
-            onTap: () {
+          // 디바운싱 구현: 탭을 빠르게 두번연속 누르면 두번째 탭이 실행되지 않게 해줌
+          return TapDebouncer(
+            onTap: () async {
               context.go('/detail/${todos[index].id}');
-              // Navigator.push(
-              //   context,
-              //   MaterialPageRoute(
-              //     builder: (context) {
-              //       return DetailPage(id: todos[index].id);
-              //     },
-              //   ),
-              // );
+              await Future.delayed(const Duration(milliseconds: 300));
             },
-            child: ToDoWidgets(id: todos[index].id),
+            builder: (context, onTap) {
+              return GestureDetector(
+                onTap: onTap,
+                child: ToDoWidgets(id: todos[index].id),
+              );
+            },
           );
         },
       ),
